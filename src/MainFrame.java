10607048ac4d95a1aaa9d5ce4685217c2228ecee
csengekulker <k71608883429@ktch.hu>
2022-05-04 11:20:27 +0200

    table filled with data

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -99,13 +99,40 @@ public class MainFrame extends JFrame {
 
   private void setTable() {
     Vector<Vector<String>> rows = new Vector<>(); 
-    Vector<String> rowitems = new Vector<>();
+    Vector<String> rowItems = new Vector<>();
     Vector<String> colNames = new Vector<>();
 
 
     colNames.add("Első");
     colNames.add("Második");
     colNames.add("Harmadik");
+
+    //row1
+    rowItems.add("row1-col1");
+    rowItems.add("row1-col2");
+    rowItems.add("row1-col3");
+
+    rows.add(rowItems);
+
+    //row2
+
+    rowItems = new Vector<>();
+
+    rowItems.add("row2-col1");
+    rowItems.add("row2-col2");
+    rowItems.add("row2-col3");
+
+    rows.add(rowItems);
+
+    //row3
+
+    rowItems = new Vector<>();
+
+    rowItems.add("row3-col1");
+    rowItems.add("row3-col2");
+    rowItems.add("row3-col3");
+
+    rows.add(rowItems);
 
     DefaultTableModel tableModel = new DefaultTableModel(
       rows, colNames
